e8aad0cb4f4d670ff1694e978bac38b71624fa1a
shar09 <sharathtelu9@gmail.com>
2021-03-04 13:40:04 -0600

    color app separated

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-
-  const [ btnStateInitial, setBtnStateInitial ] = useState(true);
-  const [ checkBoxValue, setCheckBoxValue ] = useState(false);
-  const [ btnDisabled, setBtnDisabled ] = useState(false);
-
-
-  useEffect( () => {
-    // console.log(btnDisabled);
-  }, [btnDisabled]);
-
-  const handleChange = () => {
-    setCheckBoxValue(!checkBoxValue);
-    setBtnDisabled(!btnDisabled);
-  }
-
-  const handleClick = () => {
-    setBtnStateInitial(!btnStateInitial);
-  }
-
-  return (
-    <Fragment>
-      {
-        btnStateInitial ? (
-        <button className={`${!btnDisabled ? 'btn-red': 'btn-disabled'}`}
-          onClick={handleClick}
-          disabled={btnDisabled}
-        >
-          Change to blue
-        </button>
-        ) : (
-          <button className={`${!btnDisabled ? 'btn-blue': 'btn-disabled'}`}
-            onClick={handleClick} 
-            disabled={btnDisabled}
-          >
-            Change to red
-          </button>
-        )
-      }
-
-      <input type="checkbox" 
-        id="disable-button-checkbox"
-        value={checkBoxValue} 
-        onChange={handleChange} 
-      />
-      <label htmlFor="disable-button-checkbox">Disable Button</label>
-    </Fragment>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
---
+++ src/color-app/colorapp.test.js
@@ -0,0 +1,69 @@
+import { screen, render, fireEvent } from "@testing-library/react";
+import ColorApp from "./index.js";
+
+test("color button renders and updates correctly", () => {
+  render(<ColorApp />);
+
+  // Intital render
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toHaveClass("btn-red");
+
+  // On click
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveTextContent("Change to red");
+  expect(colorButton).toHaveClass("btn-blue");
+});
+
+test("initial render for button and checkbox", () => {
+  render(<ColorApp />);
+
+  // Button enabled
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+
+  // Checkbox unchecked
+  const checkbox = screen.getByRole("checkbox", { name: "Disable Button" });
+  expect(checkbox).not.toBeChecked();
+});
+
+test("toggle button state based on checkbox value", () => {
+  render(<ColorApp />);
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByLabelText("Disable Button");
+
+  // toggle checkbox
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
+
+// test user flow
+// intial render -> red,
+// checkbox enable -> gray
+// checkbox disable -> red
+// button on click -> blue
+// checkbox enable -> gray
+// checkbox disable -> blue
+
+test("user flow", () => {
+  render(<ColorApp />);
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable Button" });
+
+  expect(colorButton).toHaveClass("btn-red");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveClass("btn-disabled");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveClass("btn-red");
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveClass("btn-blue");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveClass("btn-disabled");
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveClass("btn-blue");
+});
